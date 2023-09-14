e201c57fb75973439cc908b57e800aea25ac418e
lny926 <jaredlny@gmail.com>
2023-09-14 13:20:26 +1200

    9.14

---
+++ Assets/Scripts/TowerButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject towerPrefab;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    public Sprite Sprite
+    {
+        get
+        {
+            return sprite;
+        }
+    }
+
+    public GameObject TowerPrefab
+    {
+        get
+        {
+            return towerPrefab;
+        }
+    }
+}
