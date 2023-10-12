6e437a787d59727a77a2847a70c349ab3d31b00b
Jarrett Sanchez <133571345+jarrettsanchez@users.noreply.github.com>
2023-10-12 13:09:13 +1300

    Transported Prototype to my branch

--- Mythic Chronicles Sentinel/Assets/Scripts/ChangeScene.cs
+++ Mythic Chronicles Sentinel/Assets/Scripts/ChangeScene.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Andre
+
 public class ChangeScene : MonoBehaviour
 {
-   public void MoveToScene(int sceneID)
+    public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
     }
@@ -20,11 +24,21 @@ public class ChangeScene : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
-        PlayerPrefs.SetInt("Music Volume Changed", 0);
     }
 
-    public void Continue()
+    public void ExitStage()
     {
         Time.timeScale = 1;
+        GameObject musicObject = GameObject.FindWithTag("BackgroundMusic");
+        AudioSource audioSource = musicObject.GetComponent<AudioSource>();
+        audioSource.clip = AssetDatabase.LoadAssetAtPath<AudioClip>("Assets/Music/Fiery_Greatsword.mp3");
+        audioSource.Play();
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void RestartStage(int sceneID)
+    {
+        Time.timeScale = 1;
+        MoveToScene(sceneID);
     }
 }
